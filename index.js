2ecbac2321ef589e99d8f8d35e1486e81e85b128
sam <wyysammore@gmail.com>
2020-04-13 23:29:35 +0800

    Init 🎉🎉

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const program = require('commander');
+const downloads = require('download-git-repo');
+const inquirer = require('inquirer');
+const handlebars = require('Handlebars');
+const chalk = require('chalk');
+const ora = require('ora');
+const symbols = require('log-symbols');
+
+program
+  .version('1.0.0', '-v, --version')
+  .command('init <name>')
+  .description('初始化项目')
+  .action((name) => {
+    if (!fs.existsSync(name)) {
+      inquirer.prompt([
+        {type: 'input', name: 'author', message: '请输入创建者名称'}
+      ]).then((answers) => { 
+        const spinner = ora('正在下载模板...');
+        spinner.start();
+        downloads('https://github.com/samwangdd/vue_shoppingmall.git', name, {clone: 'true'}, (err) => {
+          if (err) {
+            spinner.fail();
+            console.log(symbols.error, chalk.red(err));
+          }else{
+            spinner.succeed();
+            const fileName = `${name}/package.json`;
+            const meta = {
+              name,
+              description: answers.description,
+              author: answers.author
+            };
+            if (fs.existsSync(fileName)) {
+              const content = fs.readFileSync(fileName).toString();
+              const result = handlebars.compile(content)(meta);
+              fs.writeFileSync(fileName, result);
+            }
+            console.log(symbols.succeed, chalk.succeed('项目初始化完成！'));
+          }
+        })
+      });
+    }else{
+      console.log(symbols.err, chalk.red('项目已存在！'))
+    }
+  })
+
+program.parse(process.argv);
