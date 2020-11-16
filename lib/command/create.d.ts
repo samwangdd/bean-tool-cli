71897d928f55f3ea1849a919fbb742257b57238a
sam <wyysammore@gmail.com>
2020-11-16 12:22:57 +0800

    feat: generator

---
+++ lib/command/create.d.ts
@@ -0,0 +1,1 @@
+export default function create(projectName: any, options: any): Promise<void>;
---
+++ lib/utils/string.d.ts
@@ -0,0 +1,2 @@
+export declare function camelize(str: any): any;
+export declare function cleanArgs(cmd: any): {};
