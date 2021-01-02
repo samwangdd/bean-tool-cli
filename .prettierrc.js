fac8d07065d2bd4b252aa063f610b542a5887f5a
王森 <wyysammore@gmail.com>
2021-01-02 21:47:15 +0800

    chore: modify git prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -7,6 +7,5 @@ module.exports = {
   trailingComma: 'all',
   bracketSpacing: true,
   arrowParens: 'avoid',
-  // Don't specify parser to make sure muti-language formatting.
-  // parser: "typescript",
+  // parser: 'typescript',
 };
