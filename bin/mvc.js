4a88a800677e35d4c543dd4ff6b047824863a588
samwangdd <wyysammore@gmail.com>
2021-06-08 10:57:56 +0800

    feat: add mvc

--- bin/mvc.js
+++ bin/mvc.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 const program = require('commander');
+const create = require('../lib/create');
 
 program
   .version('0.1.0')
   .command('create <name>')
   .description('create a new project')
-  .action(name => console.log('name :>> ', name));
+  .action(name => create(name));
 
 program.parse();
