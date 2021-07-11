599b5f9ed868267507910d4a92a51e38fe928493
samwangdd <wyysammore@gmail.com>
2021-07-11 10:43:59 +0800

    feat(utils): add readFiles

---
+++ packages/cli/lib/utils/readFiles.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const globby = require('globby');
+const { isBinaryFileSync } = require('isbinaryfile');
+const normalizeFilePaths = require('./normalizeFilePaths');
+
+module.exports = async function readFiles(context) {
+  const files = await globby(['**'], {
+    cwd: context,
+    onlyFiles: true,
+    gitignore: true,
+    ignore: ['**/node_modules/**', '**/.git/**', '**/.svn/**'],
+    dot: true,
+  });
+
+  const res = {};
+  for (const file of files) {
+    const name = path.resolve(context, file);
+    res[file] = isBinaryFileSync(name) ? fs.readFileSync(name) : fs.readFileSync(name, 'utf8');
+  }
+
+  return normalizeFilePaths(res);
+};
